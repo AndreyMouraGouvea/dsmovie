d1dd1827853f6123696cbeeea741420073fd054f
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2022-01-20 13:44:37 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+
+    return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DS Movie</h1>
+          <a href="https://github.com/AndreyMouraGouvea" target="_new">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/AndreyMouraGouvea</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+    );
+
+}
+
+export default Navbar;
